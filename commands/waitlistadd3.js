68b70168e609a76c2bfc5fb077e6628a4a66b4cd
Ronald Bruintjes <rbruintjes@medeco.com>
2012-11-01 09:27:36 -0400

    Made snide comment about using +1 and +q...

--- commands/waitlistadd3.js
+++ commands/waitlistadd3.js
@@ -7,7 +7,12 @@ exports.matchStart = false;
 exports.handler = function(data) {
     if (config.enforcement.waitlist) {
 		if (data.userid == '4e132721a3f75114de033e4b') {
-			output({text: 'Don't you mean "+1"', destination: data.source, userid: data.userid});
+			if (data.text == '+1') {
+				output({text: 'You should use "+q"', destination: data.source, userid: data.userid});
+			}
+			else {
+				output({text: 'Don\'t you mean "+1"', destination: data.source, userid: data.userid});
+			}
 		}
         addToWaitlist(data.userid, data.name, data.source);
 		
